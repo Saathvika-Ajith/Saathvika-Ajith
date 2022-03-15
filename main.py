18cbad70e21e4d8b3ae82287099c8187fe5d4fc8
Saathvika Ajith Ajith <89208817+Saathvika-Ajith@users.noreply.github.com>
2022-03-15 05:37:11 +0000

    added on

---
+++ main.py
@@ -0,0 +1,10 @@
+from working.menuClassic import print_menu2
+from working.swap import test_swappers
+from working.matrix import test_matrices
+from working.ship import ship
+
+if __name__=='__main__':
+  ship()
+  test_swappers() 
+  test_matrices()
+  print_menu2() 
